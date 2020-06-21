eb8453624b752b260386abf294169e6ba4504a6a
kimseeun <chaelsea000@gmail.com>
2020-06-21 15:52:36 +0900

    router-dom-2

---
+++ src/Components/Header.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <header>
+                <ul>
+                    <li>
+                        <a href="/">Movies</a>
+                    </li>
+                    <li>
+                        <a href="/tv">TV</a>
+                    </li>
+                    <li>
+                        <a href="/search">Search</a>
+                    </li>
+                </ul>
+            </header>
+        )
+    }
+}
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom" 
+import { HashRouter as Router, Route, Redirect, Switch } from "react-router-dom" 
+// 화면이동과 관련됨
 // {}가 있으면 뒤에서부터 적어야 자동완성, 화면을 라우팅해준다 
 // Router.js-> Controller 역할  Route->data
 import Movie from "../Routes/Movie";
 import TV from "../Routes/TV";
 import Search from "../Routes/Search";
 
+
 export default () => (
     <Router>
-        <>//view가 들어오는 칸인데 비어있는 뷰 
+        <Switch>    
+        {/* view가 들어오는 칸인데 비어있는 뷰  */}
             <Route path="/" exact component={Movie} />
             <Route path="/tv" exact component={TV} />
             <Route path="/search" exact component={Search} />
-        </>   
+            <Redirect from="*" to="/" />
+        </Switch>
     </Router>
 );
